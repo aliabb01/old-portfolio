83af570696f36888445a4dde937722a21c7e7fbc
aliabb <ali3abbasov@gmail.com>
2021-07-17 22:56:25 +0400

    add: First and second column instances to home page

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,19 +1,64 @@
 <template>
-    <div>
-        This is home page
+<!-- home-main -->
+    <main>
+        <div>
+            <v-row class="vh-100">
+                <v-col md="6" class="text-center py-10 div1" style="">
+                    <IntroFirst />
+                </v-col>
+                <!-- <div class="white-triangle"></div> -->
+                <v-col md="6" class="text-center py-10 div2">
+                    
+                    <IntroSecond />
+                </v-col>
+            </v-row>
 
-        <v-icon>
-            mdi-plus
-        </v-icon>
-    </div>
+            <IntroAvatar />
+        </div>
+    </main>
 </template>
 
 <script>
-export default {
+import IntroFirst from '../components/IntroFirst.vue';
+import IntroSecond from '../components/IntroSecond.vue';
 
+export default {
+    components: { IntroFirst, IntroSecond },
 }
 </script>
 
 <style>
+.home-main {
+    height: 89vh;
+    width: 100%;
 
+    position: relative;
+}
+
+.main-div {
+    flex: 0.5;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.div1 {
+
+}
+
+.div2 {
+    background-color: #195BDB;
+}
+
+.white-triangle {
+    background-color: white;
+    position: absolute;
+    top: 0;
+    left: 45%;
+    width:20px;
+    height: 100%;
+
+    border-bottom: 89vh solid #195BDB;
+    border-left: 100px solid transparent;
+}
 </style>
