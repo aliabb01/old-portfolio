91818b809bd917df741229e65b8ab7f07197f5d0
aliabb <ali3abbasov@gmail.com>
2021-07-17 22:52:25 +0400

    add: Projects and Contact pages with routes

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,6 +1,8 @@
 import VueRouter from 'vue-router';
 import Home from './pages/Home.vue';
 import About from './pages/About.vue';
+import Projects from './pages/Projects.vue';
+import Contact from './pages/Contact.vue';
 
 const routes = [
     {
@@ -12,6 +14,16 @@ const routes = [
         path: '/about',
         name: 'about',
         component: About,
+    },
+    {
+        path: '/projects',
+        name: 'projects',
+        component: Projects,
+    },
+    {
+        path: '/contact',
+        name: 'contact',
+        component: Contact,
     }
 ]
 
