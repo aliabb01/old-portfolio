3d014b250b2135c592e6ccbb919df9ba24b0c9b7
aliabb <ali3abbasov@gmail.com>
2021-07-17 22:54:53 +0400

    add: Introduction section columns

---
+++ resources/js/components/IntroFirst.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="mx-5">
+        <IntroAvatar />
+        <IntroDescription />
+        <IntroButton class="mt-10" />
+    </div>
+</template>
+
+<script>
+import IntroAvatar from '../components/IntroAvatar.vue';
+import IntroDescription from '../components/IntroDescription.vue';
+import IntroButton from '../components/IntroButton.vue';
+export default {
+    components: { IntroAvatar, IntroDescription, IntroButton },
+}
+</script>
+
+<style>
+
+</style>
