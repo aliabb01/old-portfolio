c1c4d66f9089103de1ef0a4a6efeee48fd4070c7
aliabb <ali3abbasov@gmail.com>
2021-07-17 22:53:29 +0400

    refactor: Update navbar to include dark theme switcher

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -1,30 +1,56 @@
 <template>
-    <div>
-        <div class="navbar">
-            <v-row class="mx-5 justify-content-around align-items-center">
-                <div class="nav-link brand text-center">
+    <div class="navbar">
+        <v-row class="justify-content-around align-items-center">
+            <div class="nav-link brand text-center">
+                <router-link to="/" >
                     <v-avatar size="5rem" color="white">
                         <img src="/images/logo.svg" alt="John" />
                     </v-avatar>
-                </div>
-                <div class="">
-                    <router-link to="/" class="navlink mx-4">Home</router-link>
-                    <router-link to="/about" class="navlink mx-4">About</router-link>
-                    <router-link to="/" class="navlink mx-4">Projects</router-link>
-                    <router-link to="/" class="navlink mx-4">Contact Me</router-link>
-                </div>
-                <div>
-                    <v-icon color="#e5e5e5" >
+                </router-link>
+            </div>
+            <div class="nav">
+                <router-link to="/" class="navlink mx-2">Home</router-link>
+                <router-link to="/about" class="navlink mx-2">About</router-link>
+                <router-link to="/projects" class="navlink mx-2">Projects</router-link>
+                <router-link to="/contact" class="navlink mx-2">Contact Me</router-link>
+            </div>
+            <div style="display: flex;" >
+                <!-- <v-btn color="transparent" elevation="2" fab>
+                    <v-icon color="#e5e5e5" size="40" >
                         mdi-moon-waning-crescent
                     </v-icon>
-                </div>
-            </v-row>
-        </div>
+                </v-btn> -->
+                
+                <v-switch
+                    inset
+                    color="yellow"
+                    v-model="darkMode"
+                >
+                </v-switch>
+                <v-icon v-if="darkMode" color="yellow" size="40" >                        
+                    <!-- mdi-moon-waning-crescent -->
+                    <!-- mdi-lightbulb-on-outline -->
+                    <!-- mdi-brightness-1 -->
+                    mdi-lightbulb
+                </v-icon>
+                <v-icon v-if="!darkMode" color="#e5e5e5" size="40">
+                    <!-- mdi-moon-waning-crescent -->
+                    mdi-lightbulb
+                </v-icon>
+                
+            </div>
+        </v-row>
     </div>
 </template>
 
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            darkMode: false
+        }
+    }
+};
 </script>
 
 <style>
@@ -35,6 +61,15 @@ export default {};
     padding-bottom: 1.0rem;
 
     background-color: #195bdb;
+
+    z-index: 99;
+}
+
+@media (min-width: 800px) {
+    .navbar {
+        position: sticky !important; 
+        top: 0 !important;
+    }
 }
 
 .navlink {
