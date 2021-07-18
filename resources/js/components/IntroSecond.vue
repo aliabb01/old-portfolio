97c3d73e6b78bc62df3d6e6685f57dee9e6d7e6f
aliabb <ali3abbasov@gmail.com>
2021-07-19 00:16:21 +0400

    refactor: Update IntroSecond

--- resources/js/components/IntroSecond.vue
+++ resources/js/components/IntroSecond.vue
@@ -1,6 +1,15 @@
 <template>
-    <div>
-        This is second intro
+    <div style="" class="d-flex flex-column justify-content-center">
+        <span class="description2">I am a web developer</span>
+
+        <div class="d-flex justify-content-center">
+            <v-img
+                src="/images/web_developer.svg"
+                max-width="45rem"
+            >
+
+            </v-img>
+        </div>
     </div>
 </template>
 
@@ -11,5 +20,13 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@1,700&display=swap');
 
+.description2 {
+    color: white;
+
+    font-size: 2rem;
+    font-style: italic;
+    font-family: 'Source Sans Pro', sans-serif;
+}
 </style>
