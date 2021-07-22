e85a7adb43092ac1fa9c5f501edbb438e85c3437
aliabb <ali3abbasov@gmail.com>
2021-07-22 23:35:17 +0400

    add: PortfolioItemPage with full initial design

--- resources/js/pages/PortfolioItemPage.vue
+++ resources/js/pages/PortfolioItemPage.vue
@@ -3,8 +3,9 @@
         <v-container>
             <v-btn
                 style="text-transform: none;"
+                class="my-5"
                 color="red lighten-1"
-                dark
+                outlined
                 @click="$router.go(-1)"
             >
                 <v-icon>
@@ -13,35 +14,67 @@
                 
                 <span class="mx-2">Back</span>
             </v-btn>
+            
+            <div class="portfolio-item-page py-5 elevation-2">
 
-            <div class="portfolio-item-page d-flex flex-column justify-content-center align-items-center my-10">
-                <h4>{{ portfolioData[id].title }}</h4>
-                <p>{{ portfolioData[id].description }}</p>
+                <div class="my-5">
+                    <h4 class="portfolio-item--title text-center">{{ portfolioData[id].title }}</h4>
+                    <p class="portfolio-item--description text-center">{{ portfolioData[id].description }}</p>
+                </div>
 
-                <div>
-                    
-
+                <div class="carouselDiv">
                     <v-carousel
                         cycle
-                        height="400"
-                        hide-delimiter-background
-                        show-arrows-on-hover
+                        style="width: 50rem; border-radius: 30px; !important"
+                        class="mx-auto"
                     >
                         <v-carousel-item
                             v-for="(slideImg, i) in portfolioData[id].carouselImg"
-                            :key="i"
+                            :key="i"                            
                         >
-                            <v-sheet                            
-                                height="100%"
+                            <v-sheet
+                                height="110%"
                             >
-                                <v-img
-                                    :src="slideImg"
-                                    width="40rem"
+                                <v-row
+                                    class="fill-height"
                                 >
-                                </v-img>
+
+                                    <v-img
+                                        :src="slideImg"
+                                        class="portfolioImageCarousel"
+                                    >
+                                    </v-img>
+
+                                </v-row>
                             </v-sheet>
                         </v-carousel-item>
                     </v-carousel>
+                </div>
+
+                <div class="container" style="text-align: left; max-width: 50rem">
+                    <div>
+                        <p class="portfolio-item--longDescription my-5" style="margin-bottom: 0px; font-size: 1.2rem">
+                            {{ portfolioData[id].longDescription }}
+                        </p>
+                    </div>
+                </div>
+
+                <div class="container " style="max-width: 50rem">
+                    <div class="mt-4">
+                        <i class="mx-2"><b>Related tags :</b></i>
+                        <v-chip-group>
+                            
+                            <v-chip
+                                v-for="(item, i) in portfolioData[id].tags"
+                                :key="i"
+                                
+                                :ripple="false"
+                                class="mx-2"
+                            >
+                                <span> {{ item }}</span>
+                            </v-chip>
+                        </v-chip-group>
+                    </div>
                 </div>
             </div>
 
@@ -56,9 +89,41 @@ export default {
     computed: {
         ...mapState(["portfolioData"])
     },
+    mounted() {
+        window.scrollTo(0, 0)
+    }
 }
 </script>
 
 <style>
+.portfolio-item-page {
+    /* background-color: rgb(220, 224, 227); */
+    border-radius: 50px;
+}
 
+.portfolio-item--title {
+    font-family: 'Source Sans Pro', sans-serif;
+    font-size: 2rem;
+}
+
+.portfolio-item--description {
+    font-family: 'Source Sans Pro', sans-serif;
+    font-size: 1rem;
+    
+    color: gray;
+}
+
+.portfolio-item--longDescription {
+    font-family: 'Source Sans Pro', sans-serif;
+}
+
+.portfolioImageCarousel {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+}
+
+#app > div > div:nth-child(2) > div > div > div:nth-child(3) > div > div.v-carousel__controls > div > button {
+    color: white;
+}
 </style>
