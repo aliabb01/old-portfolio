cee4cc9ea68061584d1bd860baa287c89c96842c
aliabb <ali3abbasov@gmail.com>
2021-07-23 23:19:48 +0400

    refactor: Update themes

--- resources/js/themes.js
+++ resources/js/themes.js
@@ -1,17 +1,21 @@
 const themes =  {
     light: {
         primary: 'rgb(255, 255, 255)',
-        secondary: 'dodgerblue',
-        primaryLight: '#1859dc20',
-        text:'rgb(10,10,10)',
-        placeholder: 'rgb(220, 224, 227)'
+        secondary: 'rgb(30, 144, 255)',       // dodgerblue
+        secondaryLight: 'rgba(30, 144, 255, 0.4)', // simple tag color
+        text: 'rgb(10,10,10)',
+        textLight: 'rgba(10, 10, 10, 0.5)',
+        placeholder: 'rgb(240, 240, 240)',
+        tag: 'gray'
     },
     dark: {
         primary: '#1F1926',
-        secondary: '#195BDB',
-        primaryLight: '#1859dc50',
-        text:'rgb(230,230,230)',
-        placeholder: 'rgb(72, 66, 79)'
+        secondary: 'rgb(25, 91, 219)',
+        secondaryLight: 'rgba(25, 91, 219, 0.4)', // facebook dark mode button color
+        text: 'rgb(230,230,230)',
+        textLight: 'rgba(230 ,230, 230, 0.5)',
+        placeholder: 'rgb(72, 66, 79)',
+        tag: 'rgba(25, 91, 219, 0.4)'
     }
 }
 
