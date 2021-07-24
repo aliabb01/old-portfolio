53eaf53c036559af6bf29092fd1085a61039fb00
aliabb <ali3abbasov@gmail.com>
2021-07-24 18:35:29 +0400

    refactor: Remove readyHandler.js

--- resources/js/helpers/readyHandler.js
+++
@@ -1,8 +0,0 @@
-const readyHandler = (loading) => {
-    if(document.readyState == 'complete') {
-        loading = false;
-        document.removeEventListener('readystatechange', readyHandler);
-    }
-};
-
-export default readyHandler
