f11e3228232bfbafa5a7123859bd31fc23deb599
aliabb <ali3abbasov@gmail.com>
2021-07-29 22:33:36 +0400

    add: ChipFeatured component for featured portfolio items

---
+++ resources/js/components/ChipFeatured.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-chip
+        v-if="featured"
+        style="position: absolute; z-index: 3; top: -1.2rem; left: -1.4rem; transition: all 0.2s; color: white"
+        :color="color"
+        :style="hover ? [ { transform: 'rotate(' + -370 + 'deg)' }, { transition: 'all 0.2s' } ] : '' "    
+    >
+        {{ text }}
+    </v-chip>
+</template>
+
+<script>
+export default {
+    props: ["featured", "color", "text", "hover"]
+};
+</script>
+
+<style></style>
