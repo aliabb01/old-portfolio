70b848d7b0f760cc592b9a0e400b24e16e216e79
aliabb <ali3abbasov@gmail.com>
2021-07-30 23:58:25 +0400

    add: TechFirst and TechSecond components to Tech

---
+++ resources/js/components/TechFirst.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div class="container">
+            <span
+                class="techLabel"
+                :style="darkTheme ? { color: darkNameLabel } : { color: lightNameLabel }"
+            >Technologies I am using</span>
+        </div>
+
+        <div class="container">
+            
+        </div>
+    </div>
+</template>
+
+<script>
+import themes from '../themes';
+import { mapState } from 'vuex';
+export default {
+    data() {
+        return {
+            lightNameLabel: themes.light.text,
+            darkNameLabel: themes.dark.text,
+            lightBg: themes.light.placeholder,
+            darkBg: themes.dark.placeholder
+        }
+    },
+    computed: {
+        ...mapState(["darkTheme"]),
+    },
+}
+</script>
+
+<style>
+.techLabel {
+    font-family: 'Source Sans Pro', sans-serif;
+    font-size: 1.5rem;
+}
+</style>
---
+++ resources/js/components/TechSecond.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+        <div
+            class="row"
+        >
+            <TechItem
+                v-for="(item, i) in techList"
+                :key="i"
+                :imgSrc="item.imgSrc"
+                :title="item.title"
+            />
+        </div>
+        
+    </div>
+</template>
+
+<script>
+import TechItem from './TechItem.vue';
+
+export default {
+    components: { TechItem },
+    data() {
+        return {
+            techList: [
+                {
+                    imgSrc: '/images/techlogos/html.svg',
+                    title: 'HTML'
+                },
+                {
+                    imgSrc: '/images/techlogos/css.svg',
+                    title: 'CSS'
+                },
+                {
+                    imgSrc: '/images/techlogos/bootstrap.svg',
+                    title: 'Bootstrap'
+                },
+                {
+                    imgSrc: '/images/techlogos/js.svg',
+                    title: 'JS'
+                },
+                {
+                    imgSrc: '/images/techlogos/jquery.svg',
+                    title: 'jQuery'
+                },
+                {
+                    imgSrc: '/images/techlogos/laravel.svg',
+                    title: 'Laravel'
+                },
+                {
+                    imgSrc: '/images/techlogos/vue.svg',
+                    title: 'Vue'
+                },
+                {
+                    imgSrc: '/images/techlogos/react.svg',
+                    title: 'React'
+                },
+                {
+                    imgSrc: '/images/techlogos/figma.svg',
+                    title: 'Figma'
+                },
+            ]
+        }
+    },
+}
+</script>
+
+<style>
+</style>
