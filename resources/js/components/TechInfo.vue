87c238a4ff2c813e25b1bd040ef03d8e867c73ea
aliabb <ali3abbasov@gmail.com>
2021-07-31 17:24:47 +0400

    refactor: Update component names
    TechFirst -> TechInfo, TechSecond -> TechLogos

---
+++ resources/js/components/TechInfo.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="techFirst--main">
+        
+
+        <div 
+            class="container"
+            style="min-height: 20rem"
+            :style="darkTheme ? { color: darkNameLabel } : { color: lightNameLabel }"
+            id="techInfoContainer"
+        >
+            <span class="techName">{{ techData[selectedTech].name }}</span>
+            <span>{{ techData[selectedTech].shortDescription }}</span>
+            <p class="techDescription">{{ techData[selectedTech].description }}</p>
+
+            <p>{{ techData[selectedTech].experience }}</p>
+        </div>
+
+    </div>
+</template>
+
+<script>
+import themes from '../themes';
+import { mapState } from 'vuex';
+export default {
+    data() {
+        return {
+            lightBg: themes.light.placeholder,
+            darkBg: themes.dark.placeholder,
+
+            lightNameLabel: themes.light.text,
+            darkNameLabel: themes.dark.text,
+        }
+    },
+    computed: {
+        ...mapState(["darkTheme", "selectedTech", "techData"]),
+    },
+}
+</script>
+
+<style>
+.techFirst--main {
+    font-family: 'Source Sans Pro', sans-serif;
+}
+
+.techName {
+    font-size: 1.8rem;
+    font-weight: 600;
+}
+
+.techDescription {
+    font-size: 1.1rem;
+}
+</style>
---
+++ resources/js/components/TechLogos.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <div
+            class="mx-2 row"
+        >
+            <TechItem
+                v-for="(item, i) in techData"
+                :key="i"
+                :imgSrc="item.imgSrc"
+                :title="item.name"
+                :index="i"
+            />
+        </div>
+        
+    </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import TechItem from './TechItem.vue';
+
+export default {
+    components: { TechItem },
+    data() {
+        return {
+            
+        }
+    },
+    computed: {
+        ...mapState(["techData"]),
+    },
+}
+</script>
+
+<style>
+</style>
