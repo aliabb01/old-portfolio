05a5824751605990f6dbef0c6e5ebb03c59beb5a
aliabb <ali3abbasov@gmail.com>
2021-08-05 22:21:11 +0400

    add: Reusable alert component

---
+++ resources/js/components/Alert.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-snackbar
+        v-model="alert"
+        :timeout="3000"
+        :color="color"
+    >
+        {{ message }}
+        <template v-slot:action="{ attrs }">
+            <v-btn
+                color="black"
+                text
+                v-bind="attrs"
+                @click="alert = false"
+            >
+                <v-icon
+                    size="20"
+                >
+                    mdi-close
+                </v-icon>
+            </v-btn>
+      </template>
+    </v-snackbar>
+</template>
+
+<script>
+export default {
+    props: ["color", "message"],
+    data() {
+        return {
+            alert: true,
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
