8ff96e71d1c4e71808871e12beeb5976e352a56d
aliabb <ali3abbasov@gmail.com>
2021-08-06 22:26:40 +0400

    refactor: Update js experience level

--- resources/js/techData.js
+++ resources/js/techData.js
@@ -68,7 +68,7 @@ const techData = [
         name: 'JavaScript',
         description: 'JavaScript is a text-based programming language used both on the client-side and server-side that allows you to make web pages interactive. Where HTML and CSS are languages that give structure and style to web pages, JavaScript gives web pages interactive elements that engage a user',
         experience: jsExperience,
-        experienceLevel: 75
+        experienceLevel: 74
     },
     {
         id: 5,
