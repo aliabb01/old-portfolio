25d5093351f953d92fb363b58858641d022661d2
aliabb <ali3abbasov@gmail.com>
2021-08-07 21:58:12 +0400

    add: Self image with hover 3d effect and gradient border

--- resources/js/components/IntroAvatar.vue
+++ resources/js/components/IntroAvatar.vue
@@ -1,20 +1,34 @@
 <template>
     <div>
         <v-avatar
-            style="border-width: 7px; border-style: solid"
-            :style="darkTheme ? { borderColor: darkAvatarCircle } : { borderColor: lightAvatarCircle }"
+            style="border-style: solid;"
+            :style="'backgroundImage: ' + (darkTheme ? darkAvatarGradient : lightAvatarGradient)"
             size="200"
-            class="shadow"
+            class="shadow introAvatar--border"
         >
             <v-avatar
-                size="170"
-                color="#C4C4C4" 
+                class="introAvatar--img"
+                size="180"
+                :color="darkTheme ? darkAvatar : lightAvatar"
             >
-                <span>Pic</span>
-                <!-- <img
-                    src="/images/some_image_here"
-                    alt="John"
-                > -->
+                <!-- <span>Pic</span> -->
+                <v-img
+                    class="selfImage"
+                    src="/images/self.jpg"
+                    alt="Ali Abbasov"
+                    onContextMenu="return false;"
+                    max-width="160"
+                    max-height="160"
+                >
+                <template v-slot:placeholder>
+                    <v-skeleton-loader
+                        :loading="true"
+                        class="mx-auto"
+                        type="image"
+                        :dark="dark"
+                    ></v-skeleton-loader>
+                </template>
+                </v-img>
             </v-avatar>
         </v-avatar>
     </div>
@@ -26,8 +40,12 @@ import { mapState } from 'vuex';
 export default {
     data() {
         return {
+            lightAvatar: themes.light.primary,
+            darkAvatar: themes.dark.primary,
             lightAvatarCircle: themes.light.secondary,
-            darkAvatarCircle: themes.dark.secondary
+            darkAvatarCircle: themes.dark.secondary,
+            lightAvatarGradient: themes.light.secondaryGradient,
+            darkAvatarGradient: themes.dark.secondaryGradient
         }
     },
     computed: {
@@ -37,5 +55,18 @@ export default {
 </script>
 
 <style>
+.introAvatar--border {
+    background-origin: border-box;
+    background-clip: content-box, border-box;
+}
 
+.introAvatar--img .selfImage {
+    transition: all 1s;
+}
+
+.introAvatar--img:hover .selfImage {
+    cursor: pointer;
+    transform: rotate3d(1, 1, 1, 360deg);
+    transition: all 1s;
+}
 </style>
