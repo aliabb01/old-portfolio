995676a3bf24e151d0b3c27bded072fe66840b3c
aliabb <ali3abbasov@gmail.com>
2021-08-09 10:33:26 +0400

    add: SocialLink component for single link

---
+++ resources/js/components/SocialLink.vue
@@ -0,0 +1,53 @@
+<template>
+    <v-hover v-slot="{ hover }">
+        <a class="socialLink" target="blank" :title="tooltip" :href="href">
+            <v-icon
+                :color="darkTheme ? darkTextLabel : lightTextLabel"
+                size="50"
+                class="socialLink--icon"
+                style="text-decoration: none;"
+            >
+                mdi-{{ icon }}
+            </v-icon>
+            <span class="socialLink--text"
+                :style="'color: ' + (hover ? (darkTheme ? lightSecondary : darkSecondary) : (darkTheme ? darkTextLabel : lightTextLabel)) + ';'"
+            >{{ text }}</span>
+        </a>
+    
+    </v-hover>
+</template>
+
+<script>
+import themes from '../themes';
+import { mapState } from 'vuex';
+
+export default {
+    props: ["tooltip", "href", "icon", "text"],
+    data() {
+        return {
+            lightTextLabel: themes.light.text,
+            darkTextLabel: themes.dark.text,
+            lightSecondary: themes.light.secondary,
+            darkSecondary: themes.dark.secondary,
+        }
+    },
+    computed: {
+        ...mapState(["darkTheme"])
+    }
+}
+</script>
+
+<style>
+.socialLink:hover {
+    text-decoration: none;
+}
+
+.socialLink--text {
+    font-size: 1.1rem;
+    text-decoration: none;
+}
+
+/* .socialLink:hover .socialLink--text {
+    color: blue !important;
+} */
+</style>
